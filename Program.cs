6981dc8986b54be5b867235fa6f58e819933b545
chistyakovdennis <chistyakov.d@gmail.com>
2023-07-25 19:31:51 +0300

    Вариант решения

--- Program.cs
+++ Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+// Эта программа сначала позволяет пользователю ввести элементы массива с клавиатуры через запятую. 
+// Затем она создает новый массив, содержащий только те строки, которые имеют длину, меньшую или равную 3 символам, 
+// и выводит результат на экран.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод массива с клавиатуры
+        Console.Write("Введите элементы массива через запятую: ");
+        string input = Console.ReadLine();
+
+        string[] inputArray = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Формирование нового массива с длиной строк <= 3 символа
+        string[] newArray = FilterArray(inputArray);
+
+        // Вывод результата
+        Console.WriteLine("\nРезультат:");
+        foreach (string item in newArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static string[] FilterArray(string[] inputArray)
+    {
+        int count = 0;
+
+        // Подсчет количества строк, удовлетворяющих условию
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создание нового массива с соответствующими строками
+        string[] newArray = new string[count];
+        int index = 0;
+
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                newArray[index] = item;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+}
